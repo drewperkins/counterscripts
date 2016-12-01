2e1253c200d3337a36473ef3a5095346a5a61c0c
drewperkins <drew.a.perkins@gmail.com>
2016-11-30 20:23:52 -0800

    Initial commit

---
+++ traildata.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 30 20:02:52 2016
+
+@author: Drew
+"""
+
+import json
+import matplotlib.pyplot as plt
+from dateutil import parser
+
+
+
+with open('/Users/Drew/Downloads/trail.json') as f:
+    data = f.read()
+    json_data = json.loads(data)
+
+counts = []
+timestamps = []
+counter1 = json_data['CounterData']['TrailCounter_1']
+for k,v in counter1.items():
+    print('Time: ' + v['ts'])
+    timestamps.append(parser.parse(v['ts']))
+    counts.append(v['Count'])
+    print('Count: ' + v['Count'])
+
+plt.plot(timestamps, counts, 'ro')
+plt.ylabel('some numbers')
+plt.show()
